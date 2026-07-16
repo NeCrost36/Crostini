bcea1af90608a0b6988858ce27b692c5ad10ab08
necrost36 <1120151209@qq.com>
2026-07-16 16:45:58 +0800

    Update Version.cs

--- Crostini/Crostini/Version.cs
+++ Crostini/Crostini/Version.cs
@@ -1,14 +1,22 @@
-using System;
-
 namespace Crostini
 {
-    public class Version
+    public readonly struct Version
     {
-        public struct Version
-        {
-            public int Major;public int Minor;public int Patch;
-            public int hotfix;public int fix;
-        }
-    }
+        public int Major { get; }
+        public int Minor { get; }
+        public int Patch { get; }
+        public int Hotfix { get; }
+        public int Build { get; }  
 
+        public Version(int major, int minor, int patch, int hotfix = 0, int build = 0)
+        {
+            Major = major;
+            Minor = minor;
+            Patch = patch;
+            Hotfix = hotfix;
+            Build = build;
+        }
+
+        public override string ToString() => $"{Major}.{Minor}.{Patch}.{Hotfix}.{Build}";
+    }
 }
